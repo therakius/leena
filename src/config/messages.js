66b3d3907f29f2cdfcbbd20f50caf3d767f818b4
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2026-04-02 19:10:27 +0200

    create message config

---
+++ src/config/messages.js
@@ -0,0 +1,6 @@
+export const messages = {
+    greeting: 'Olá! Eu sou a Leena, sua assistente de vendas. Como posso ajudar hoje?',
+    mistake: `EN: I'm creating a bot, you received this message by mistake...\n\nPT: Estou a criar um bot, recebeste essa mensagem por engano...`,
+    notUnderstood: 'Desculpe, não entendi. Pode reformular a pergunta?',
+    goodbye: 'Até logo! Volte sempre que precisar. 👋',
+}
