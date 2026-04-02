8214d78011461888b800c1519fee854a44f2abb2
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2026-04-02 17:20:49 +0200

    create db connection

---
+++ src/config/db.js
@@ -0,0 +1,11 @@
+// connecting the database
+import pkg from 'pg';
+import dotenv  from 'dotenv';
+dotenv.config()
+
+const {Client: PGClient} = pkg;
+
+export const pg = new PGClient({
+    connectionString: process.env.DATABASE_URL
+})
+
