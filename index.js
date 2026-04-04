7268e1a23678e72c4762b2c1a6d1cb3b9d61f24a
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2026-04-04 17:27:19 +0200

    chore: add heartbeat to mantain connection alive

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-import { pg } from './src/config/db.js';
+import { pg, startHeartbeat } from './src/config/db.js';
 import { setupTable } from './src/services/sessionService.js';
 import { startBot } from './src/services/whatsappService.js';
 
 pg.connect()
-    .then(setupTable)
-    .then(startBot)
-    .catch(console.error);
+  .then(() => {
+    console.log('Connected to the database');
+
+    startHeartbeat(); // inicia heartbeat
+
+    return setupTable();
+  })
+  .then(startBot)
+  .catch(console.error);
+
+pg.on('error', (err) => {
+  console.error('Unexpected DB error:', err);
+});
--- src/config/db.js
+++ src/config/db.js
@@ -5,6 +5,20 @@ dotenv.config();
 const { Client: PGClient } = pkg;
 
 export const pg = new PGClient({
-    connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-// await pg.connect()
+
+// função heartbeat
+export const startHeartbeat = (intervalMs = 4 * 60 * 1000) => {
+  setInterval(async () => {
+    try {
+      await pg.query('SELECT 1'); // mantém a conexão viva
+      // console.log('DB heartbeat OK');
+    } catch (err) {
+      console.error('DB heartbeat falhou:', err);
+    }
+  }, intervalMs);
+};
